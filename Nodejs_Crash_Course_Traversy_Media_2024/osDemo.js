4e45fb0f0df4b873576decb307aa6f96556bf9c4
Manav Sheth <46321809+manavsheth@users.noreply.github.com>
2024-06-05 16:45:54 +0200

    Delete Nodejs_Crash_Course_Traversy_Media_2024 directory

--- Nodejs_Crash_Course_Traversy_Media_2024/osDemo.js
+++
@@ -1,9 +0,0 @@
-import os from 'os'; // This module provides a number of operating system-related utility methods. 
-//userInfo()
-console.log(os.userInfo());
-//totalmem()
-console.log(os.totalmem());
-//freemem();
-console.log(os.freemem());
-//cpus
-console.log(os.cpus());
